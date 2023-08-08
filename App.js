88272710299b560d3f51c43653ce5a7f08a22261
Aman8167 <118344734+Aman8167@users.noreply.github.com>
2023-08-08 11:21:40 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,71 +1,16 @@
-import React from 'react';
+import React from 'react'
 import './App.css';
+import Customcard from './Component/Customcard';
 
 
 function App() {
   return (
-    <div className="container-fluid">
-      <br></br>
-    <div className="row justify-content-center">
-
-      <div className="col-md-11">
-        <div className="custom-card ml-4">
-        <div class="row">
-        <div className="col-sm-5">
-               <div class="card-body">
-                <p>
-                  Title-<br></br>
-                  Description-<br></br>
-                  Applicable booking-<br></br>
-                  Discount-
-                </p>
-               </div>
-            </div>
-            <div className="col-sm-5">
-               <div class="card-body1">
-               <button type="button" class="btn btn-info">Edit Top Service</button>
-               </div>
-            </div>
-        </div>
-          
-        </div>
-      </div>
-      
+     
+      <div className='app'>
  
-      
-    </div>
-    <br></br><br>
-    </br>
-    <div className="row justify-content-center">
-
-      <div className="col-md-11">
-        <div className="custom-card ml-4">
-        <div class="row">
-        <div className="col-sm-5">
-               <div class="card-body">
-                <p>
-                  Title-<br></br>
-                  Description-<br></br>
-                  Applicable booking-<br></br>
-                  Discount-
-                </p>
-               </div>
-            </div>
-            <div className="col-sm-5">
-               <div class="card-body1">
-               <button type="button" class="btn btn-info">Edit Top Service</button>
-               </div>
-            </div>
-        </div>
-          
-        </div>
+  <Customcard />
       </div>
-      
- 
-      
-    </div>
-    
-  </div>
+
   );
 }
 
---
+++ Customcard.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import './Custom.css';
+function Customcard() {
+  return (
+    <div className="container-fluid">
+      <br></br>
+    <div className="row justify-content-center">
+
+      <div className="col-md-12">
+        <div className="custom-card ml-4">
+        <div class="row">
+        <div className="col-sm-5">
+               <div class="card-body">
+                <p>
+                  Title-<br></br>
+                  Description-<br></br>
+                  Applicable booking-<br></br>
+                  Discount-
+                </p>
+               </div>
+            </div>
+            <div className="col-sm-5">
+               <div class="card-body1">
+               <button type="button" class="btn btn-info">Edit Top Service</button>
+               </div>
+            </div>
+        </div>
+          
+        </div>
+      </div>
+      
+ 
+      
+    </div>
+    <br></br><br>
+    </br>
+    <div className="row justify-content-center">
+
+      <div className="col-md-12">
+        <div className="custom-card ml-4">
+        <div class="row">
+        <div className="col-sm-5">
+               <div class="card-body">
+                <p>
+                  Title-<br></br>
+                  Description-<br></br>
+                  Applicable booking-<br></br>
+                  Discount-
+                </p>
+               </div>
+            </div>
+            <div className="col-sm-5">
+               <div class="card-body1 ">
+               <button type="button" class="btn btn-info">Edit Top Service</button>
+               </div>
+            </div>
+        </div>
+          
+        </div>
+      </div>
+      
+ 
+      
+    </div>
+    
+  </div>
+  )
+}
+
+export default Customcard;
